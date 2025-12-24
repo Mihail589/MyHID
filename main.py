687d08fbd9899a34e40b182f46692471b4113a90
Mihail589 <lyapkin12@list.ru>
2025-12-24 21:05:22 +0300

    4

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ class Hid(BaseHid):
 
     def _open_path(self, path):
         self.device = hid.device()
+        attrs = dir(self.device)
+        print(attrs)
         self.device.open_path(path)
-        print(self.device.fd)
+        
         
     def write(self, data):
         self.device.write(data)
@@ -20,7 +22,7 @@ class Hid(BaseHid):
         return data
 if __name__ == "__main__":
     print(Hid.discover())
-    h = Hid.init_by_device_name("TBS CROSSFIRE")
+    h = Hid.init_by_device_name("Usb Mouse")
     h.run()
     for i in range(3):
         h.write(b'\xc8\x04(\x00\x0e|')
