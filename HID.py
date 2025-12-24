0c8b70e2f6e88d79066ac9a3d4afd27c01e5d75f
Mihail589 <lyapkin12@list.ru>
2025-12-24 21:44:29 +0300

    7

--- HID.py
+++ HID.py
@@ -1,4 +1,4 @@
-import hid, select
+import hid, select, glob, os
 
 from base_hid import *
 
@@ -10,8 +10,32 @@ class Hid(BaseHid):
     def _open_path(self, path):
         self.device = hid.device()
         self.device.open_path(path)
+        vid, pid = 0, 0
+        for i in self.discover():
+            if i["path"] == path:
+                vid = i["vendor_id"]
+                pid = i["product_id"]
         
+        if vid == 0:
+            return
         
+        # Ищем hidraw
+        vid_hex = f"{vid:04x}".lower()
+        pid_hex = f"{pid:04x}".lower()
+        
+        for hidraw in glob.glob('/dev/hidraw*'):
+            try:
+                name = os.path.basename(hidraw)
+                uevent = f'/sys/class/hidraw/{name}/device/uevent'
+                
+                if os.path.exists(uevent):
+                    with open(uevent, 'r') as f:
+                        if f"0003:{vid_hex}:{pid_hex}" in f.read().lower():
+                            self.select_fd = os.open(hidraw, os.O_RDONLY | os.O_NONBLOCK)
+                            print(hidraw)
+                            break
+            except:
+                continue
     def write(self, data):
         self.device.write(data)
 
